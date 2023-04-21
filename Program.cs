294217c041a8d58a6acc2b2e1b691a373a8e9d68
César Bartolomé Navarro <cesarbartolomenavarro2399@gmail.com>
2023-04-21 17:12:02 +0100

    Creado el menu responsive del inicio

--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=platos}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
