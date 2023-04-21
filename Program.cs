c2f0ec443832a326132a1fcb8b68f982b0648e09
César Bartolomé Navarro <cesarbartolomenavarro2399@gmail.com>
2023-04-21 15:45:01 +0100

    Arreglada conexion a base de datos

--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=platos}/{action=Index}/{id?}");
 
 app.Run();
